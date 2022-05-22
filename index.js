57c015895ae05efcaba013edc2ff3c449d1b6667
Shwanees <73402777+sonishn213@users.noreply.github.com>
2022-05-22 21:05:38 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let slideIndex = 1;
-
+let qty = 0;
+let cartqty = 0;
+let cart = [];
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -44,21 +46,85 @@ function closeLightBox() {
 
 function increaseQty() {
   const qtySpan = document.querySelector("#quantity");
-  let qty = qtySpan.innerHTML;
+
+  qty = qtySpan.innerHTML;
   qty++;
   qtySpan.innerHTML = qty;
-  console.log(qty);
 }
 function decreaseQty() {
   const qtySpan = document.querySelector("#quantity");
+
   let qty = qtySpan.innerHTML;
   if (qty > 0) {
     qty--;
     qtySpan.innerHTML = qty;
-    console.log(qty);
   }
 }
 
+function addToCart() {
+  if (qty > 0) {
+    let cartItem = {};
+    cartqty += qty;
+    let existingitem = cart.find((obj) => obj.id === 1);
+
+    let existingQty = 0;
+    if (existingitem) {
+      existingQty = existingitem.qty;
+      existingQty += qty;
+      let totalPrice1 = 125.0 * existingQty;
+      cart.find((obj) => obj.id === 1).qty = existingQty;
+      cart.find((obj) => obj.id === 1).finalPrice = totalPrice1;
+    } else {
+      let totalPrice2 = 125.0 * qty;
+      cartItem = {
+        id: 1,
+        img: "images/image-product-1-thumbnail.jpg",
+        pName: "Fall Limited Edition Sneaker",
+        price: "125.00",
+        qty: qty,
+        finalPrice: totalPrice2,
+      };
+      cart.push(cartItem);
+    }
+
+    const qtySpan = document.querySelector("#quantity");
+    qtySpan.innerHTML = 0;
+    qty = 0;
+    console.log(cart);
+  }
+}
+setInterval(() => {
+  if (cart.length > 0) {
+    const cartBody = document.querySelector(".cart-body");
+    const cartlist = document.querySelector(".cart-items-list");
+    const cartBadge = document.querySelector(".badge");
+
+    cartBody.classList.add("hasItem");
+    cartBadge.setAttribute("value", cartqty);
+    cartBadge.classList.add("hasItem");
+    let cartItem = cart.map((item) => {
+      return ` <div class="cart-item">
+      <div class="cart-item-image"> 
+        <img src="${item.img}">
+      </div>
+      <div class="cart-item-info">
+        <div class="cart-item-info-title">
+          ${item.pName}
+        </div>
+        <div class="cart-item-info-prices">
+          <span class="prices-left"
+            ><span>$${item.price}</span><span>x</span><span>${item.qty}</span></span
+          ><span class="prices-right">$${item.finalPrice}</span>
+        </div>
+      </div>
+      <div class="cart-item-delete" onclick="removeItemCart()">
+        <img src="images/icon-delete.svg" alt="" />
+      </div>
+    </div>`;
+    });
+    cartlist.innerHTML = cartItem;
+  }
+}, 500);
 function openSidebar() {
   const mainmenu = document.querySelector(".mobile-menu");
   const sideBar = document.querySelector(".sidebar");
@@ -72,4 +138,17 @@ function closeSidebar() {
 function showCart() {
   const cartDisplay = document.querySelector(".cart-list-container");
   cartDisplay.classList.toggle("show");
+}
+function removeItemCart() {
+  const cartBody = document.querySelector(".cart-body");
+  const cartlist = document.querySelector(".cart-items-list");
+  const cartBadge = document.querySelector(".badge");
+
+  cartBody.classList.remove("hasItem");
+  cartBadge.setAttribute("value", 0);
+  cartBadge.classList.remove("hasItem");
+  cart = [];
+  if (cart.length <= 0) {
+    cartlist.innerHTML = "";
+  }
 }
